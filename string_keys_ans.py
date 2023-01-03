6c87d11a51c736b6c0dd5f9128cb95b650b64987
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-01-03 12:56:14 +0800

    finish string keys

--- string_keys_ans.py
+++ string_keys_ans.py
@@ -7,42 +7,71 @@ import string
 
 class HashTable(object):
     def __init__(self):
-        self.table = [None]*700
+        self.table = [None]*10000
 
-        # MAY ALSO CREATE BUCKET DURING ADDITION OF WORD A WORD
-        # Create buckets for the hash table (First two letters of a word)
+        # MAY ALSO CREATE BUCKET DURING ADDITION OF WORD (THAT IS FASTER IF HASH TABLE IS GOING TO RESET/DELETED OFTEN)
+        # Create buckets for the hash table (key is ascii value of first two letter of a word)
+        # Counter for the index of the buckets
         counter = 0
+        # Loop through the alphabets twice
         for i in string.ascii_uppercase:
             for j in string.ascii_uppercase:
-                self.table[counter] = {"key": i+j, "words": []}
+                # Create a dictionary/bucket for each letter permutation (two letter)
+                # The key is the combination of the ascii value of the two letter while the values is a list of words starting with the two letters
+                self.table[counter] = {"key": str(ord(i))+str(ord(j)), "words": []}
                 counter += 1
 
     def store(self, string):
         """Input a string that's stored in 
         the table."""
+        # Calculate the hash value of the given string
+        hash_value = self.calculate_hash_value(string)
+        # Loop through the hash table to find the correct bucket
         for bucket in self.table:
+            # Some buckets are unused, so disregard if bucket is None
             if bucket:
-                if bucket["key"] == string[:2].upper():
+                # If bucket's key matches the hash value of the string then add the given string to the words list of that bucket
+                if bucket["key"] == hash_value:
                     bucket["words"].append(string)
-                    # print(self.table)
-                    return
+                    break
         else:
-            print("ERROR: probably used a non alphabet character")
+            # If no matching hash value for that string then there is an error (commented print is used to submit in udacity since f string don't work there)
+            print(f"ERROR: no buckets for '{string}', probably used a non alphabet character")
+            # print("ERROR: no buckets for %s, probably used a non alphabet character" %string)
 
-
-            
 
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
-        return -1
+        # Calculate the hash value of the given string
+        hash_value = self.calculate_hash_value(string)
+        # Loop through the hash table to find the correct bucket
+        for bucket in self.table:
+            # Some buckets are unused, os disregard if bucket is None
+            if bucket:
+                # If bucket's key matches the hash value of the string then check if the given string is in word list of that bucket
+                if bucket["key"] == hash_value:
+                    if string in bucket["words"]:
+                        return hash_value
+                    else:
+                        return -1
+        else:
+            # If no matching hash value for that s
+            print(f"ERROR: no buckets for '{string}', probably used a non alphabet character")
+            # print("ERROR: no buckets for %s, probably used a non alphabet character" %string)
+
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
-        return -1
-    
+        hash_value = ""
+        # Get the ascii value of the first two letter of the given string, then combine them
+        for index, i in enumerate(string[:2]):
+            hash_value += str(ord(i))
+        return hash_value
+
+
 # Setup
 hash_table = HashTable()
 
