d7a443010d302a628844a5029f660eb06ccf26a6
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-01-03 19:43:02 +0800

    finish binary search tree

--- binary_search_tree_ans.py
+++ binary_search_tree_ans.py
@@ -4,35 +4,55 @@ class Node(object):
         self.left = None
         self.right = None
 
-    def __str__ (self):
-        return f"{self.value}"
-
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-
     def insert(self, new_val):
+        # Start with the root
         current = self.root
-        while new_val < current.value:
-            if current.left:
-                current = current.left
-            else:
-                current.left = Node(new_val)
-                break
-            
-        while new_val > current.value:
-            if current.right:
-                current = current.right
-            else:
-                current.right = Node(new_val)
-                break
-                
-        print(current.value, current.left, current.right)
+        # Loop until the proper location of the new node is found 
+        while True:
+            # If curernt value is less then given value, then it should be in the left
+            if current.value > new_val:
+                # If there is a node in the left, then go to that node
+                if current.left:
+                    current = current.left
+                # If no node, then that position is perfect for the new node
+                else:
+                    current.left = Node(new_val)
+                    break
+            # if current value is greater than given value, then it should be in the right
+            elif current.value < new_val:
+                # If there is a node in the right, then go to that node
+                if current.right:
+                    current = current.right
+                # If no node, then that position is perfect for the new node
+                else:
+                    current.right = Node(new_val)
+                    break
+            # Catches error, incase user tried to enter a repeat value
+            elif current.value == new_val:
+                print("ERROR: number already in binary tree")
 
     def search(self, find_val):
-        return False
-    
+        # Start with the root
+        current = self.root
+        # Loop until the the given value is found, or that it is not found in its appropriate location
+        while True:
+            # It is found, if current value is same as given value 
+            if current.value == find_val:
+                return True
+            # If current value is greater than given value, then go to left node if it exists
+            elif current.value > find_val and current.left:
+                current = current.left
+            # If courrent value is less than given value, then go to the right node if it exists
+            elif current.value < find_val and current.right:
+                current = current.right
+            # If not found in its appropriate location then it does not exist
+            else:
+                return False
+
 # Set up tree
 tree = BST(4)
 
